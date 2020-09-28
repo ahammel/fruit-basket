0df103bc46b85a229213eb2f707c9924b78bcc94
Alex Hammel <ahammel87@gmail.com>
2020-09-28 09:43:22 -0700

    🌚 Creatio ex nihilo

---
+++ doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to fruit-basket
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ test/fruit_basket/core_test.clj
@@ -0,0 +1,7 @@
+(ns fruit-basket.core-test
+  (:require [clojure.test :refer :all]
+            [fruit-basket.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
